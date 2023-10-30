1743ffad1a96897ec939a1a1d1e29c1eaf839f34
mandaragnihotri14@gmail.com <mandaragnihotri14@gmail.com>
2023-10-30 16:30:57 +0530

    VPC creation added

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -21,15 +21,45 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   role       = aws_iam_role.example.name
 }
 
+
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+  tags = {
+   Name = "Project VPC"
+ }
+}
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+resource "aws_subnet" "public_subnets" {
+ count             = length(var.public_subnet_cidrs)
+ vpc_id            = aws_vpc.main.id
+ cidr_block        = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+  tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+
 #get vpc data
-data "aws_vpc" "default" {
+data "aws_vpc" "main" {
   default = true
 }
 #get public subnets for cluster
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    values = [data.aws_vpc.main.id] 
   }
 }
 #cluster provision
