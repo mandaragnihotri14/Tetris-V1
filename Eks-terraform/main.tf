3673fe988d2eea41ddef09d5ce1e49340cf4a94c
mandaragnihotri14@gmail.com <mandaragnihotri14@gmail.com>
2023-11-08 16:14:40 +0530

    Modules and veriable file added

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -1,133 +1,32 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
-  tags = {
-   Name = "Project VPC"
- }
-}
-
-variable "public_subnet_cidrs" {
- type        = list(string)
- description = "Public Subnet CIDR values"
- default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "azs" {
- type        = list(string)
- description = "Availability Zones"
- default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-resource "aws_subnet" "public_subnets" {
- count             = length(var.public_subnet_cidrs)
- vpc_id            = aws_vpc.main.id
- cidr_block        = element(var.public_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
-  tags = {
-   Name = "Public Subnet ${count.index + 1}"
- }
+module "vpc" {
+  source              = "./modules/VPC"
+  region              = var.region
+  vpccidr             = var.vpccidr
+  public_subnet_cidrs = var.public_subnet_cidrs
+  azs                 = var.azs
 }
 
 #get vpc data
-data "aws_vpc" "main" {
-  default = true
-}
+#data "aws_vpc" "main" {
+#  default = true
+#}
+
 #get public subnets for cluster
-data "aws_subnets" "public_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.main.id] 
-  }
+#data "aws_subnets" "public" {
+#  filter {
+#    name   = "vpc-id"
+#    values = [data.aws_vpc.main.id]
+#  }
+#}
+
+module "eks" {
+  source = "./modules/EKS"
+  region = var.region
+  cluster_name = var.cluster_name
+  subnet_ids = module.vpc.subnet_id
+  node_group_name = var.node_group_name
+  eks_iam_rolename = var.eks_iam_rolename
+  eks_nodegroup_iam_rolename = var.eks_nodegroup_iam_rolename
 }
 
-#cluster provision
-resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
-  role_arn = aws_iam_role.example.arn
-
-  vpc_config {
-    subnet_ids = tolist(data.aws_subnets.public_subnets.ids)
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-  ]
-}
-
-resource "aws_iam_role" "example1" {
-  name = "eks-node-group-cloud"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example1.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example1.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example1.name
-}
-
-#create node group
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "Node-cloud"
-  node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids      = data.aws_subnets.public_subnets.ids
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
-  instance_types = ["t2.medium"]
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
-  ]
-}
